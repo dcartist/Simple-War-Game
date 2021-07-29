6c6d4d26e65a6beac2dfa11165373709322f11e3
dcartist <pbannerman13@gmail.com>
2021-07-29 18:18:27 -0400

    updating router on index

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 // import './App.css';
 import './Style.scss'
 import Card from "./Components/Card/Card"
-
+import React, { useState } from 'react';
 function App() {
+  setSta
+  
   return (
     <div className="App">
       <Card></Card>
