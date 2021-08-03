25a4b1a00d0132f1823ec734e86d62c200dca20e
dcartist <pbannerman13@gmail.com>
2021-08-03 01:03:03 -0400

    updating style

--- src/Pages/Game.js
+++ src/Pages/Game.js
@@ -39,8 +39,8 @@ export default class Game extends Component {
     
         if (this.props.playerDeck.length === 0 && this.props.warCheck === false){
         return(
-            <div>
-                You haven't seleted cards, please return to select your cards
+            <div className="default-background blue-background centered instruction">
+                <p>You haven't seleted cards, please return to select your cards</p>
                 <ReturntoDeck></ReturntoDeck>
             </div>
         )
@@ -53,8 +53,13 @@ export default class Game extends Component {
                     <h3>Player's card</h3>
                     <Card deckStyle={this.props.deckStyle + 3} rank={this.props.playerDeck[0].rank} suit={this.props.playerDeck[0].suit}/>
                     </div>
-                  <div className="card-placement"> WAR!
-                    <button onClick={this.activateWar}>Enter the War</button></div>
+                  <div className="card-placement"> 
+                    <button onClick={this.activateWar}>Enter the War</button>
+                    
+                    <p>TIED!</p>
+                    <p>Player Cards: {this.props.playerDeck.length}</p>
+                    <p>Computer's Cards: {this.props.computerDeck.length}</p>
+                    </div>
                   <div className="card-placement">
                     <h3>Computer's card</h3>
                   <Card deckStyle={this.props.deckStyle + 3} rank={this.props.computerDeck[0].rank} suit={this.props.computerDeck[0].suit}/>
@@ -65,14 +70,15 @@ export default class Game extends Component {
 
         } else if (this.props.playerDeck.length == this.props.endNumber && this.props.warCheck === false) {
             return (
-                <div className="default-background">
+                <div className="default-background blue-background centered winner">
                     <h1>Player is winner of the War!</h1>
+                    <Link to="/"><button>Click here to return to intro</button></Link>
                 </div>
             )
         }
          else if (this.props.endNumber == this.props.computerDeck.length && this.props.warCheck === false) {
         return (
-            <div className="default-background red-background centered">
+            <div className="default-background blue-background centered winner">
                 <h1>The Computer is winner of the War!</h1>
                 <Link to="/"><button>Click here to return to intro</button></Link>
 
@@ -87,10 +93,9 @@ export default class Game extends Component {
                     <Card deckStyle={this.props.deckStyle} rank={this.props.playerDeck[0].rank} suit={this.props.playerDeck[0].suit}/>
                     </div>
                     <div className="card-placement">
-
                     <button onClick={this.war}>War</button>
-                    <p>Winner of round: {this.props.winnerOfRound}</p>
-                    <p> Player Cards:{this.props.playerDeck.length}</p>
+                    <p>Winner of round:<br></br> {this.props.winnerOfRound}</p>
+                    <p> Player Cards: {this.props.playerDeck.length}</p>
                     <p>Computer's Cards: {this.props.computerDeck.length}</p>
                     </div>
                   <div className="card-placement">
